e8601fe535baabd1ece939d6160b30cf267ac3ef
Azentro <info@azentrogs.com>
2026-03-19 19:55:49 +0400

    Fix hero responsive layout

--- app/services/engineering/page.tsx
+++ app/services/engineering/page.tsx
@@ -121,7 +121,7 @@ export default function EngineeringServices() {
             
             <div
               key={index}
-              className="md:sticky md:top-10 w-full"
+              className="sticky md:top-10 w-full"
               style={{
                 zIndex: 20 + index,
                     }}
